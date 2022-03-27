1dc8ed4c3e9a6b345b466a521a54b048fad89899
firstChairCoder <md1qq7@gmail.com>
2022-03-27 22:45:02 +0100

    Initial boilerplate setup.

--- src/Authentication/index.tsx
+++ src/Authentication/index.tsx
@@ -1,21 +1,21 @@
-import React from "react";
-import { createStackNavigator } from "@react-navigation/stack";
+//import React from "react";
+//import { createStackNavigator } from "@react-navigation/stack";
 
-import type { Routes } from "../components/Navigation";
+//import type { Routes } from "../components/Navigation";
 
-import Onboarding, { assets as onboardingAssets } from "./Onboarding";
-import Welcome, { assets as welcomeAssets } from "./Welcome";
-import Login from "./Login";
-export const assets = [...onboardingAssets, ...welcomeAssets];
+//import Onboarding, { assets as onboardingAssets } from "./Onboarding";
+//import Welcome, { assets as welcomeAssets } from "./Welcome";
+//import Login from "./Login";
+//export const assets = [...onboardingAssets, ...welcomeAssets];
 
-const AuthenticationStack = createStackNavigator<Routes>();
+//const AuthenticationStack = createStackNavigator<Routes>();
 
-export const AuthenticationNavigator = () => {
-  return (
-    <AuthenticationStack.Navigator screenOptions={{ headerShown: false }}>
-      <AuthenticationStack.Screen name={"Onboarding"} component={Onboarding} />
-      <AuthenticationStack.Screen name={"Welcome"} component={Welcome} />
-      <AuthenticationStack.Screen name={"Login"} component={Login} />
-    </AuthenticationStack.Navigator>
-  );
-};
+//export const AuthenticationNavigator = () => {
+//  return (
+   // <AuthenticationStack.Navigator screenOptions={{ headerShown: false }}>
+    //  <AuthenticationStack.Screen name={"Onboarding"} component={Onboarding} />
+  //    <AuthenticationStack.Screen name={"Welcome"} component={Welcome} />
+  //    <AuthenticationStack.Screen name={"Login"} component={Login} />
+//    </AuthenticationStack.Navigator>
+//  );
+//};
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,1 @@
 export { default as LoadAssets } from "./LoadAssets";
-export { default as Button } from "./Button";
-export { default as theme, Text } from "./Theme";
-export { default as Container } from "./Container";
